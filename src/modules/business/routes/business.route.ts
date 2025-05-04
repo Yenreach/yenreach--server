149c380d6d186926270bca5818b6babebeaa4070
Lankiman <marvellousgboun@gmail.com>
2025-05-04 14:48:51 +0100

    fix: removed auth middlware for categories

--- src/modules/business/routes/business.route.ts
+++ src/modules/business/routes/business.route.ts
@@ -23,9 +23,9 @@ class BusinessRoute implements Routes {
       next();
     });
 
-    this.router.get(`/states`, authMiddleware, this.businessController.getStates);
-    this.router.get(`/states/:id/lgas`, authMiddleware, this.businessController.getLga);
-    this.router.get(`${this.path}/categories`, authMiddleware, this.businessController.getCategories);
+    this.router.get(`/states`, this.businessController.getStates);
+    this.router.get(`/states/:id/lgas`, this.businessController.getLga);
+    this.router.get(`${this.path}/categories`, this.businessController.getCategories);
     this.router.get(`${this.path}/:id/related`, this.businessController.getRelatedBusiness);
     this.router.get(`${this.path}/:id`, this.businessController.getBusiness);
     this.router.get(`${this.path}`, this.businessController.getBusinesses);
