1f4cf0abe867ec64b52026d2bd7943eb9e805c40
Lankiman <marvellousgboun@gmail.com>
2025-08-11 14:39:35 +0100

    fix:products fix

--- src/modules/products/controllers/products-admin.controller.ts
+++ src/modules/products/controllers/products-admin.controller.ts
@@ -1,20 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
-import { ProductAdminService } from '../services';
+
 import { sendResponse } from '../../../lib/utils';
 import { HttpCodes } from '../../../lib/constants';
 import { CreateBlackFridayDealDto, GetProductsDto, UpdateBlackFridayDealDto } from '../schemas/products.schema';
+import { ProductAdminService } from '../services';
 
+const productsAdminService = new ProductAdminService();
 class ProductsAdminController {
-  private readonly productsAdminService: ProductAdminService;
-
-  constructor() {
-    this.productsAdminService = new ProductAdminService();
-  }
+  constructor() {}
 
   public async createBlackFridayProducts(req: Request, res: Response, next: NextFunction) {
     try {
       const productsData: CreateBlackFridayDealDto = req.body;
-      const newProducts = await this.productsAdminService.createBlackFridayDeal(productsData);
+      const newProducts = await productsAdminService.createBlackFridayDeal(productsData);
       return sendResponse(res, HttpCodes.CREATED, 'Product created successfully', newProducts);
     } catch (error) {
       next(error);
@@ -24,7 +22,7 @@ class ProductsAdminController {
   public async getBlackFridayDealById(req: Request, res: Response, next: NextFunction) {
     try {
       const dealId = req.params.id;
-      const deal = await this.productsAdminService.getBlackFridayDealById(dealId);
+      const deal = await productsAdminService.getBlackFridayDealById(dealId);
       if (!deal) return sendResponse(res, HttpCodes.NOT_FOUND, 'Black Friday deal not found');
       return sendResponse(res, HttpCodes.OK, 'Black Friday deal retrieved successfully', deal);
     } catch (error) {
@@ -35,7 +33,7 @@ class ProductsAdminController {
   public async getBlackFridayDeals(req: Request, res: Response, next: NextFunction) {
     try {
       const query: GetProductsDto = req.query;
-      const deals = await this.productsAdminService.getBlackFridayDeals(query);
+      const deals = await productsAdminService.getBlackFridayDeals(query);
       return sendResponse(res, HttpCodes.OK, 'Black Friday deals retrieved successfully', deals);
     } catch (error) {
       next(error);
@@ -46,7 +44,7 @@ class ProductsAdminController {
     try {
       const page = Number(req.query.page) || 1;
       const limit = Number(req.query.limit) || 10;
-      const deals = await this.productsAdminService.getAllBlackFridayDeals(page, limit);
+      const deals = await productsAdminService.getAllBlackFridayDeals(page, limit);
       return sendResponse(res, HttpCodes.OK, 'All Black Friday deals retrieved successfully', deals);
     } catch (error) {
       next(error);
@@ -57,7 +55,7 @@ class ProductsAdminController {
     try {
       const dealId = req.params.id;
       const data: UpdateBlackFridayDealDto = req.body;
-      const updatedDeal = await this.productsAdminService.updateBlackFridayDeal(dealId, data);
+      const updatedDeal = await productsAdminService.updateBlackFridayDeal(dealId, data);
       return sendResponse(res, HttpCodes.OK, 'Black Friday deal updated successfully', updatedDeal);
     } catch (error) {
       next(error);
@@ -67,7 +65,7 @@ class ProductsAdminController {
   public async deleteBlackFridayDeal(req: Request, res: Response, next: NextFunction) {
     try {
       const dealId = req.params.id;
-      const deleted = await this.productsAdminService.deleteBlackFridayDeal(dealId);
+      const deleted = await productsAdminService.deleteBlackFridayDeal(dealId);
       if (!deleted) return sendResponse(res, HttpCodes.NOT_FOUND, 'Black Friday deal not found');
       return sendResponse(res, HttpCodes.NO_CONTENT, 'Black Friday deal deleted successfully');
     } catch (error) {
--- src/modules/products/controllers/products.controller.ts
+++ src/modules/products/controllers/products.controller.ts
@@ -5,17 +5,14 @@ import { HttpCodes } from '../../../lib/constants';
 import { HttpException } from '../../../lib/exceptions';
 import { AddCategoryDto, AddProductCategoryDto, CreateProductDto, GetProductsSchema, UpdateProductDto } from '../schemas/products.schema';
 
+const productsService = new ProductsService();
 class ProductsController {
-  private readonly productsService: ProductsService;
-
-  constructor() {
-    this.productsService = new ProductsService();
-  }
+  constructor() {}
 
   public async createProducts(req: Request, res: Response, next: NextFunction) {
     try {
       const productsData: CreateProductDto = req.body;
-      const newProducts = await this.productsService.createProduct(productsData);
+      const newProducts = await productsService.createProduct(productsData);
 
       return sendResponse(res, HttpCodes.CREATED, 'product created successfully', newProducts);
     } catch (error) {
@@ -27,7 +24,7 @@ class ProductsController {
     try {
       const productId = req.params.id;
       const updateData: UpdateProductDto = req.body;
-      const updatedProduct = await this.productsService.updateProduct(productId, updateData);
+      const updatedProduct = await productsService.updateProduct(productId, updateData);
       return sendResponse(res, HttpCodes.OK, 'product updated successfully', updatedProduct);
     } catch (error) {
       next(error);
@@ -39,7 +36,7 @@ class ProductsController {
       const page = parseInt(req.query.page as string, 10) || 1;
       const limit = parseInt(req.query.limit as string, 10) || 10;
 
-      const products = await this.productsService.getAllProducts(page, limit);
+      const products = await productsService.getAllProducts(page, limit);
 
       return sendResponse(res, HttpCodes.OK, 'products fetched successfully', products);
     } catch (error) {
@@ -49,7 +46,7 @@ class ProductsController {
 
   public async getCategories(req: Request, res: Response, next: NextFunction) {
     try {
-      const categories = await this.productsService.getProductCategories();
+      const categories = await productsService.getProductCategories();
       return sendResponse(res, HttpCodes.OK, 'Categories fetched successfully', categories);
     } catch (error) {
       next(error);
@@ -59,7 +56,7 @@ class ProductsController {
   public async getProducts(req: Request, res: Response, next: NextFunction) {
     try {
       const queryParams = GetProductsSchema.parse(req.query);
-      const result = await this.productsService.getProducts(queryParams);
+      const result = await productsService.getProducts(queryParams);
 
       return sendResponse(res, HttpCodes.OK, 'Products retrieved successfully', result);
     } catch (error) {
@@ -74,7 +71,7 @@ class ProductsController {
 
       if (!business) throw Error('Business ID must be sent');
 
-      const result = await this.productsService.getProducts({ ...queryParams, business });
+      const result = await productsService.getProducts({ ...queryParams, business });
 
       return sendResponse(res, HttpCodes.OK, 'Products retrieved successfully', result);
     } catch (error) {
@@ -85,7 +82,7 @@ class ProductsController {
   public async getRelatedProducts(req: Request, res: Response, next: NextFunction) {
     try {
       const productId = req.params.id;
-      const products = await this.productsService.getRelatedProducts(productId);
+      const products = await productsService.getRelatedProducts(productId);
       return sendResponse(res, HttpCodes.OK, 'related products fetched successfully', products);
     } catch (error) {
       next(error);
@@ -95,7 +92,7 @@ class ProductsController {
   public async getProductById(req: Request, res: Response, next: NextFunction) {
     try {
       const productId = req.params.id;
-      const products = await this.productsService.getProductById(productId);
+      const products = await productsService.getProductById(productId);
       if (!products) {
         throw new HttpException(HttpCodes.NOT_FOUND, 'product do not exists');
       }
@@ -108,7 +105,7 @@ class ProductsController {
   public async deleteProduct(req: Request, res: Response, next: NextFunction) {
     try {
       const productId = req.params.id;
-      const result = await this.productsService.deleteProduct(productId);
+      const result = await productsService.deleteProduct(productId);
       return sendResponse(res, HttpCodes.OK, 'product deleted successfully', result);
     } catch (error) {
       next(error);
@@ -117,7 +114,7 @@ class ProductsController {
 
   public async addPhoto(req: Request, res: Response, next: NextFunction) {
     try {
-      const result = await this.productsService.addPhoto(req.body);
+      const result = await productsService.addPhoto(req.body);
       return sendResponse(res, HttpCodes.CREATED, 'Photo added successfully', result);
     } catch (error) {
       next(error);
@@ -127,7 +124,7 @@ class ProductsController {
   public async createCategory(req: Request, res: Response, next: NextFunction) {
     try {
       const data: AddCategoryDto = req.body;
-      const result = await this.productsService.createCategory(data);
+      const result = await productsService.createCategory(data);
 
       return sendResponse(res, HttpCodes.CREATED, 'category CREATED successfully', result);
     } catch (error) {
@@ -138,7 +135,7 @@ class ProductsController {
   public async addProductCategory(req: Request, res: Response, next: NextFunction) {
     try {
       const data: AddProductCategoryDto = req.body;
-      const result = await this.productsService.addProductCategory(data);
+      const result = await productsService.addProductCategory(data);
 
       return sendResponse(res, HttpCodes.CREATED, 'category added successfully', result);
     } catch (error) {
